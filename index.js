ecbf78409b3f451ec6276fa81aa362fbc84c7820
hnovara <her.novara@hotmail.com>
2024-04-22 10:27:07 -0300

    FIX index.js prompt

--- index.js
+++ index.js
@@ -1,84 +1,85 @@
 import fs, { constants } from "node:fs";
 import { Buffer } from "node:buffer";
-import prompt from "prompt-sync";
-
+import prompt from 'prompt-sync';
 import colors from "colors";
-
 
 const promptFunc = prompt();
 
-const createFile = (fileName, content) => {
+const createFile = () => {
+    const fileName = promptFunc(colors.blue("Ingrese el nombre del archivo a crear: "));
+    const content = promptFunc(colors.blue("Ingrese el contenido del archivo: "));
     const dirName = `./files/${fileName}`;
     const buffer = Buffer.from(content);
     
-    fs.writeFileSync(dirName, buffer, error => {
-        if (error) {
-            console.log(colors.red("Lo sentimos, no hemos podido crear el archivo"));
-        } else {
-            console.log(colors.green("El archivo se ha creado correctamente"));
-        }
-    });
+    try {
+        fs.writeFileSync(dirName, buffer);
+        console.log(colors.green("El archivo se ha creado correctamente"));
+    } catch (error) {
+        console.log(colors.red("Lo sentimos, no hemos podido crear el archivo"));
+    }
 };
 
-const readFile = (fileName) => {
+const readFile = () => {
+    const fileName = promptFunc(colors.blue("Ingrese el nombre del archivo a leer: "));
     const dirName = `./files/${fileName}`;
 
-    fs.access(dirName, constants.R_OK, error => {
-        if (error) {
-            console.log(colors.red("Este archivo no se puede leer"));
-        } else {
-            const file = fs.readFileSync(dirName);
-            const data = file.toString();
-            console.log(colors.green(data));
-        }
-    });
+    try {
+        const file = fs.readFileSync(dirName);
+        const data = file.toString();
+        console.log(colors.green(data));
+    } catch (error) {
+        console.log(colors.red("Este archivo no se puede leer"));
+    }
 };
 
-const updateFile = (fileName, content) => {
+const updateFile = () => {
+    const fileName = promptFunc(colors.blue("Ingrese el nombre del archivo a modificar: "));
+    const content = promptFunc(colors.blue("Ingrese el nuevo contenido del archivo: "));
     const dirName = `./files/${fileName}`;
     const buffer = Buffer.from(content);
     
-    fs.access(dirName, constants.R_OK | constants.W_OK, error => {
-        if (error) {
-            console.log(colors.red("Este archivo no se puede leer y/o no se puede escribir"));
-        } else {
-            fs.writeFileSync(dirName, buffer, error => {
-                if (error) {
-                    console.log(colors.red("Lo sentimos, no hemos podido modificar el archivo"));
-                } else {
-                    console.log(colors.green("El archivo se ha modificado correctamente"));
-                }
-            });
-        }
-    });
+    try {
+        fs.writeFileSync(dirName, buffer);
+        console.log(colors.green("El archivo se ha modificado correctamente"));
+    } catch (error) {
+        console.log(colors.red("Lo sentimos, no hemos podido modificar el archivo"));
+    }
 };
 
-const deleteFile = (fileName) => {
+const deleteFile = () => {
+    const fileName = promptFunc(colors.blue("Ingrese el nombre del archivo a eliminar: "));
     const dirName = `./files/${fileName}`;
 
-    fs.rm(dirName, error => {
-        if (error) {
-            console.log(colors.red("Lo sentimos, no hemos podido eliminar el archivo"));
-        } else {
-            console.log(colors.green("El archivo se ha eliminado correctamente"));
-        }
-    });
+    try {
+        fs.rmSync(dirName);
+        console.log(colors.green("El archivo se ha eliminado correctamente"));
+    } catch (error) {
+        console.log(colors.red("Lo sentimos, no hemos podido eliminar el archivo"));
+    }
 };
 
-const processCommand = (action, fileName, content) => {
+export const fileController = () => {
+    let action;
+    do {
+        action = promptFunc(colors.blue("Qué acción desea realizar? (Lectura: R, Edición: E, Creación: C, Eliminar: D, Salir: Q): "));
+        action = action.toUpperCase(); 
+    } while (!(action === "R" || action === "E" || action === "C" || action === "D" || action === "Q"));
+
     switch (action) {
-        case "C":
-            createFile(fileName, content);
-            break;
         case "R":
-            readFile(fileName);
+            readFile();
             break;
-        case "U":
-            updateFile(fileName, content);
+        case "E":
+            updateFile();
+            break;
+        case "C":
+            createFile();
             break;
         case "D":
-            deleteFile(fileName);
+            deleteFile();
             break;
+        case "Q":
+            return;
         default:
             console.log(colors.red("Acción no válida"));
             break;
@@ -86,18 +87,7 @@ const processCommand = (action, fileName, content) => {
 };
 
 const main = () => {
-    const args = process.argv.slice(2);
-
-    if (args.length < 2) {
-        console.log(colors.red("Se requieren al menos dos argumentos: acción y nombre del archivo"));
-        return;
-    }
-
-    const action = args[0].toUpperCase();
-    const fileName = args[1];
-    const content = args[2] || "";
-
-    processCommand(action, fileName, content);
+    fileController();
 };
 
 main();
